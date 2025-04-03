5e783d975a0b2d2c635b84bf5a4d411bb61b6674
fderue <francois.xavier.derue@gmail.com>
2025-04-03 12:49:10 +0200

    temporary commit to set a reset point

---
+++ js/nav-sidebar.js
@@ -0,0 +1,43 @@
+
+
+function fillNavigationListWithHeaders(){
+  const menu = document.getElementById("sidebar-menu");
+  const sections = document.querySelectorAll("section[id]");
+  sections.forEach((section)=>{
+    // Find header content
+    const header = section.querySelector("h2, h3");
+    const listItem = document.createElement("li");
+    listItem.style.marginLeft = (header.tagName=="H3")? "12px" : "0px";
+    const itemRef = document.createElement("a");
+    itemRef.href = `#${section.id}`;
+    itemRef.textContent = header.textContent;
+    listItem.append(itemRef);
+    menu.append(listItem);
+  });
+}
+
+function addActiveClassIfOnScreen(){
+  window.addEventListener('DOMContentLoaded', () => {
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const id = entry.target.getAttribute('id');
+        if (entry.intersectionRatio > 0) {
+          document.querySelector(`li a[href="#${id}"]`).parentElement.classList.add('active');
+        } else {
+          document.querySelector(`li a[href="#${id}"]`).parentElement.classList.remove('active');
+        }
+      });
+    });
+  
+    // Track all sections that have an `id` applied
+    document.querySelectorAll('section[id]').forEach((section) => {
+      observer.observe(section);
+    });
+    
+  });
+}
+
+fillNavigationListWithHeaders();
+addActiveClassIfOnScreen();
+
